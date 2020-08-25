6c116e806337f601ac5e9b9201cea795eb05e205
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-25 14:26:55 -0700

    Change inside room

--- src/views/Inside.js
+++ src/views/Inside.js
@@ -14,9 +14,10 @@ const Wall = Block;
 const Pedestal = Block;
 
 export default () => {
-    const size1 = 120;
-    const size2 = 120;
+    const size1 = 500;
+    const size2 = 500;
     const bottom = -27;
+    const offset = 15;
     return (
         <ViewLayer backgroundColor="rgb(140, 140, 255)" zIndex={600}>
             <Route
@@ -26,7 +27,7 @@ export default () => {
             />
             <Canvas
                 camera={{
-                    position: new Vector3(-28, bottom + 22, 0)
+                    position: new Vector3(-2, 0, 0)
                 }}
             >
                 <ambientLight intensity={0.5} />
@@ -60,8 +61,8 @@ export default () => {
                     depth={size1 * 2}
                 />
                 <Wall
-                    color="rgb(0, 168, 224)"
-                    x={0}
+                    color="rgb(100, 168, 224)"
+                    x={offset}
                     y={-size1 / 4}
                     z={0}
                     length={size2}
@@ -83,12 +84,13 @@ export default () => {
                     depth={size1 * 2}
                     width={size1 / 2}
                 />
-                <Pointer to="/inside/pin1" x={23} y={bottom} z={-18} />
-                <Pointer to="/inside/pin2" x={40} y={bottom} z={5} />
+                <Pointer to="/inside/pin1" x={23} y={bottom + offset} z={-18} />
+                <Pointer to="/inside/pin2" x={110} y={bottom} z={15} />
+                <Pointer to="/inside/pin3" x={70} y={bottom} z={50} />
                 <Pedestal
                     color="rgb(50, 50, 50)"
                     x={30}
-                    y={bottom}
+                    y={bottom + offset}
                     z={-10}
                     depth={5}
                     width={5}
@@ -97,7 +99,7 @@ export default () => {
                 <Pedestal
                     color="rgb(50, 50, 50)"
                     x={35}
-                    y={bottom}
+                    y={bottom + offset}
                     z={0}
                     depth={5}
                     width={5}
@@ -106,7 +108,7 @@ export default () => {
                 <Pedestal
                     color="rgb(50, 50, 50)"
                     x={30}
-                    y={bottom}
+                    y={bottom + offset}
                     z={10}
                     depth={5}
                     width={5}

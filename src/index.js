02288239f943a2cb78c4b8806af30a2f4751972a
yvesgurcan <gurcan.yves+github@gmail.com>
2020-08-25 14:37:53 -0700

    Restore menus

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './views/Home';
 import Inside from './views/Inside';
 import PinModal from './views/PinModal';
+import Menu1 from './views/Menu1';
+import Menu2 from './views/Menu2';
 
 const rootElement = document.getElementById('root');
 const customHistory = createBrowserHistory();
@@ -19,7 +21,11 @@ ReactDOM.render(
                 return <Home />;
             }}
         />
-        <Route path="/inside" component={Inside} />
+        <Switch>
+            <Route path="/inside" component={Inside} />
+            <Route path="/menu1" component={Menu1} />
+            <Route path="/menu2" component={Menu2} />
+        </Switch>
         <Route path="/inside/pin1" render={() => <PinModal text="1" />} />
         <Route path="/inside/pin2" render={() => <PinModal text="2" />} />
         <Route path="/inside/pin3" render={() => <PinModal text="3" />} />
